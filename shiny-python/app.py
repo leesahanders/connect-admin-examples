f0cca50f35a3781397198e8845478b9fca25232c
leesahanders <lisa.anders@rstudio.com>
2023-07-05 09:11:23 -0500

    Adding Roger's geyser example

--- shiny-python/app.py
+++ shiny-python/app.py
@@ -1,17 +1,35 @@
+## Thanks Roger!! https://github.com/lagerratrobe/R_Data/blob/master/app.py
+
+import matplotlib.pyplot as plt
+import pandas
 from shiny import App, render, ui
 
 app_ui = ui.page_fluid(
-    ui.input_slider("n", "N", 0, 100, 20),
-    ui.output_text_verbatim("txt"),
+    # App title ----
+    ui.panel_title("Old Faithful Geyser Data"),
+    ui.layout_sidebar(
+        ui.panel_sidebar(
+            ui.input_slider("n", "Number of bins:", 1, 50, 30),
+        ),
+        ui.panel_main(
+            ui.output_plot("histogram"),
+        ),
+    ),
 )
+
 
 def server(input, output, session):
     @output
-    @render.text
-    def txt():
-        return f"n*2 is {input.n() * 2}"
+    @render.plot(alt="A histogram")
+    def histogram():
+        df = pandas.read_csv("https://raw.githubusercontent.com/lagerratrobe/R_Data/master/faithful.csv")
+        x = df["waiting"]
+        plt.title("Histogram of waiting times")
+        plt.hist(x, input.n(), density=True)
+        plt.xlabel("Waiting time to next eruption (in mins)")
+        plt.ylabel("Frequency")
 
-app = App(app_ui, server)
+app = App(app_ui, server, debug=True)
 
 if __name__ == "__main__":
     app.run()
